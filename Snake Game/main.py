c2320a23875692954a18871c4fd313b40eacd1dc
Sadra <alizadeh.sadra.gh@gmail.com>
2024-09-04 20:31:47 +0300

    Progressing_20(8)

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,28 +1,32 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(False)
 
 x_position = [0, -20, -40]
+
+segments = []
 
 for snake_num in range(3):
     snake = Turtle(shape="square")
     snake.color("white")
-    snake.goto(x=x_position[snake_num], y=0 )
+    snake.penup()
+    snake.goto(x=x_position[snake_num], y=0)
+    segments.append(snake)
 
-
-
-
-
-
-
-
-
-
-
-
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 
 screen.exitonclick()
